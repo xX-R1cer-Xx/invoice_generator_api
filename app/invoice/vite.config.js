6f7ebb9edbabf92422f531276207c8080f7792d8
Yusuke Harada <90469yusukeharada@gmail.com>
2023-04-19 12:21:26 +0900

    initial commit

---
+++ app/invoice/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    server: {
+        host: true,
+        hmr: {
+            host: "localhost",
+        },
+    },
+    plugins: [
+        laravel({
+            input: "resources/ts/app.tsx",
+            refresh: true,
+        }),
+        react(),
+    ],
+});
