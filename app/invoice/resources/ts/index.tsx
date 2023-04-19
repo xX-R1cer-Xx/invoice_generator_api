86b6306b1f0970c26eb36041ac57347f4257ad71
Yusuke Harada <90469yusukeharada@gmail.com>
2023-04-19 16:27:03 +0900

    fix

--- app/invoice/resources/ts/index.tsx
+++ app/invoice/resources/ts/index.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+// https://reactjs.org/docs/react-dom-client.html
+import * as ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById("app");
-const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-
-root.render(
-    <React.StrictMode>
-        <div className="text-red">Hello World</div>
-    </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+// https://blog.logrocket.com/how-to-use-typescript-with-react-18-alpha/
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App/>);
